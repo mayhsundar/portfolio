41eb439df9d8ffb072e909998656d8d474f91dbe
Shyam Sundar <shyamsundarhacker@gmail.com>
2020-09-12 21:02:28 +0530

    personal portfolio

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -66,7 +66,7 @@ class Header extends Component {
           <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}</h1>
             <h3>
-              I'm a {city} based <span>{occupation}</span>. {description}.
+              I'm an Indian based <span>{occupation}</span>. {description}.
             </h3>
             <hr />
             <ul className="social">{networks}</ul>
