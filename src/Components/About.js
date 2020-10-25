6998cf1702be6f35ae3340a0c9ea721a6523bfb4
Shyam Sundar <shyamsundarhacker@gmail.com>
2020-10-25 23:20:22 +0530

    added rel for href new tab for security

--- src/Components/About.js
+++ src/Components/About.js
@@ -48,7 +48,12 @@ class About extends Component {
               </div>
               <div className="columns download">
                 <p>
-                  <a href={resumeDownload} target="_blank" className="button">
+                  <a
+                    href={resumeDownload}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="button"
+                  >
                     <i className="fa fa-download"></i>Download Resume
                   </a>
                 </p>
